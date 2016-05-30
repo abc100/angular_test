6da7aa7e9b98c6a98449aaa0456033595a280aed
brintsev.aleksandr <brintsev.aleksandr@gmail.com>
2016-05-30 18:35:51 +0300

    Events.

---
+++ public_html/app/js/controllers/questionController.js
@@ -0,0 +1,44 @@
+var myApp = angular.module("myApp");
+myApp.controller("questionController", function($scope) {
+    $scope.question = {
+        text: 'What framework is better to use?',
+        author: 'Vasya',
+        date: '20/10/2013',
+        answers: [
+            {
+                text: 'AngularJS',
+                author: 'Phillipp',
+                date: '21/10/2013',
+                rate: 2
+            },
+            {
+                text: 'backbone',
+                author: 'Ivan',
+                date: '22/10/2013',
+                rate: 1
+            },
+            {
+                text: 'none',
+                author: 'Pafnutij',
+                date: '21/10/2013',
+                rate: 3
+            },
+        ]
+    };
+    
+    $scope.voteUp = function(answer) {
+        answer.rate++;
+    }
+    
+    $scope.voteDown = function(answer) {
+        answer.rate--;
+    }
+    
+    $scope.questColorClass = 'questcolor';
+    
+    $scope.changeClass = function(e) {
+        $scope.questColorClass = e.type == "mouseover" ? "questselectedclass" : "questcolor";
+    }
+});
+
+
